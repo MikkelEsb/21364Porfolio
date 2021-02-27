dcf1d4b4705ecfdc0471d9149f882c355bd635b8
MikkelEsb <55182735+mikkelesb@users.noreply.github.com>
2021-02-27 02:14:47 +0100

    Added TriangleTest for centroid, area and perimeter. Although one mathematical deviation is found in the area formula

---
+++ tests/TriangleTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TriangleTest {
+    Triangle triangleA = new Triangle(new Point2D(0,0),new Point2D(1,0),new Point2D(1,1));
+    @Test
+    void centroidLocation(){
+        assertEquals(triangleA.getCenter().getX(),2/3d);
+        assertEquals(triangleA.getCenter().getY(),1/3d);
+    }
+    @Test
+    void triangleArea(){
+        assertEquals(0.5d,triangleA.getArea());
+    }
+    @Test
+    void trianglePerimeter(){
+        assertEquals(3.4142135623730950488016887242097d,triangleA.getPerimeter());
+    }
+
+}
